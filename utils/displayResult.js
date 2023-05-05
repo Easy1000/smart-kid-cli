9a85d5e08afbc53d8ba8822a77eb5c77c374a52a
easy <izaaz240@gmail.com>
2023-05-05 23:13:46 +0700

    feat(app): retry question
    player will retry the same question if they got it wrong

--- utils/displayResult.js
+++ utils/displayResult.js
@@ -3,9 +3,14 @@ import gradient from "gradient-string";
 import quizSelector from "../lib/quizSelector.js";
 
 export default async function displayResult(result) {
-  figlet(result ? "Correct" : "Wrong", (err, data) => {
+  console.clear();
+
+  figlet(result ? "Correct, keep going!" : "Wrong, retry!", (err, data) => {
     console.log(gradient.instagram.multiline(data));
   });
 
-  return quizSelector();
+  if (result)
+    setTimeout(() => {
+      quizSelector();
+    }, 500);
 }
